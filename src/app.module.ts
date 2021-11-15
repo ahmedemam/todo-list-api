659e32dbbd6544564f9addbaefec588b2e9cd753
Ahmed Emam <emam@cloudypedia.com>
2021-11-15 23:16:10 +0300

    init project modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
+import { AuthModule } from './auth/auth.module';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, TodoModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
