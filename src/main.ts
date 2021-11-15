c5badce507e34ec9eb2b2477003a7f00a9405000
Ahmed Emam <emam@cloudypedia.com>
2021-11-15 23:24:47 +0300

    add swagger module and switch express adapter to fastify adapter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );
+
+  const config = new DocumentBuilder()
+  .setTitle('Test Api documents')
+  .setDescription('Test API description')
+  .setVersion('1.0')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
